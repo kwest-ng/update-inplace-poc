680a62de1be3cacd5f06d294ac91cc6b6a751a81
Wes Van Melle <van.melle.wes@gmail.com>
2023-06-01 18:00:51 -0700

    only deploy minimal conditions

--- deploy-attempt.sh
+++ deploy-attempt.sh
@@ -5,6 +5,5 @@ mkdir -p /mnt/hostshare/scratch
 rm -vf /mnt/hostshare/scratch/*
 cp -v target/x86_64-pc-windows-gnu/debug/*.exe /mnt/hostshare/scratch/
 cd /mnt/hostshare/scratch
-echo "Setting up initial conditions"
-cp -v new-version.exe replace.exe.new
-cp -v replace.exe replace.exe.bak
+echo "Setting up minimal conditions"
+mv -v replace.exe replace.exe.bak
